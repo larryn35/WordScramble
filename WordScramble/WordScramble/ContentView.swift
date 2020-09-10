f5bd67c61c1c08d6f864298a84fa47cb23a5b5bb
Larry N <larryn35@gmail.com>
2020-09-09 21:18:18 -0700

    initial commit

---
+++ WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Larry Nguyen on 9/9/20.
+//  Copyright © 2020 Larry Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
